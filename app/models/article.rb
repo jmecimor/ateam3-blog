ff8f1555083492691ea9c21a0a84994c9d3b4d45
sam B <sbuahin@uncc.edu>
2016-10-07 09:35:16 +0000

    Completed 6.3

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ActiveRecord::Base
 #end
 #class Article < ApplicationRecord
-     validates :title, presence: true,
+      has_many :comments
+  validates :title, presence: true,
                     length: { minimum: 5 }
 end
